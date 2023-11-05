5cd3541feb3b6363dc9a0d2c51a34ff20c501582
Lucien Lin <luciensday@gmail.com>
2023-11-05 22:54:03 +0000

    a big change here: change ul li to input to be able to edit on javascript

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,34 @@
+// document.addEventListener('DOMContentLoaded', function () {
+//     const todoInput = document.getElementById('todoInput');
+//     const todoList = document.getElementById('todoList');
+
+//     todoInput.addEventListener('keyup', function (event) {
+//         if (event.key === 'Enter' && todoInput.value.trim() !== '') {
+//             addTodoItem(todoInput.value);
+//             todoInput.value = '';
+//         }
+//     });
+
+//     function addTodoItem(text) {
+//         const listItem = document.createElement('li');
+//         listItem.innerHTML = `
+//             <div class="circle"></div>
+//             <span class="todo-text">${text}</span>
+//         `;
+//         todoList.appendChild(listItem);
+//         listItem.querySelector('.circle').addEventListener('click', toggleComplete);
+//     }
+
+//     function toggleComplete() {
+//         const circle = this;
+//         const listItem = circle.parentElement;
+//         listItem.classList.toggle('complete');
+//         listItem.querySelector('.circle').classList.toggle('complete-circle');
+//         listItem.querySelector('.todo-text').classList.toggle('complete-text');
+//     }
+// });
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const todoInput = document.getElementById('todoInput');
     const todoList = document.getElementById('todoList');
@@ -13,7 +44,8 @@ document.addEventListener('DOMContentLoaded', function () {
         const listItem = document.createElement('li');
         listItem.innerHTML = `
             <div class="circle"></div>
-            <span class="todo-text">${text}</span>
+            <input class="todo-text" type=""text value="${text}" disable>
+            
         `;
         todoList.appendChild(listItem);
         listItem.querySelector('.circle').addEventListener('click', toggleComplete);
@@ -26,8 +58,4 @@ document.addEventListener('DOMContentLoaded', function () {
         listItem.querySelector('.circle').classList.toggle('complete-circle');
         listItem.querySelector('.todo-text').classList.toggle('complete-text');
     }
-
-    // write a function - "completed item automatic push to the top of list"
-    // write a function - "the completed items can be toggle to hide or show"
-    // write a fucntion - "the completed items to move to the top  "
 });
--- javascript/test1.js
+++ javascript/test1.js
@@ -1,1 +1,6 @@
-test 
+
+// Lucien trial to use TDD method to build function 
+
+test("completed items to push to the top of list ", ()=>{
+const result = completedToTop()})
+
