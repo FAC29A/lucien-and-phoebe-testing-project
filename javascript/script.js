446e11ff98ae1a1484e146ebb88978e48df7cc2d
Lucien Lin <luciensday@gmail.com>
2023-11-08 02:17:18 +0000

    hide show function

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,27 @@
     // this is actually the to do input     change the variable name later 
     const createBtnElement = document.getElementById('todoInput');
 
-    
+    let defaultHideCompleted = true; // A flag to set the default state to hide completed
+
+    // Show/hind text in button to be able clicked and toggle
+    showHideCompleted.addEventListener('click', function(){
+        if (showHideCompleted.textContent == "show completed"){
+            showHideCompleted.textContent = "hide completed"; 
+             showCompleted(); 
+        }
+            else {
+            showHideCompleted.textContent = "show completed"; 
+            hideCompleted(); 
+        }
+    }
+    )
+
+     // adjust input's textarea element height based on content text
+     createBtnElement.addEventListener('input', function(){
+        this.style.height = "auto"; 
+        this.style.height = this.scrollHeight + 'px'; 
+    })
+
     
     let toDo = [];
 
@@ -26,12 +46,16 @@
 
         const { itemElement, inputElement } = CreateTodoElement(item);
     
-        listElement.prepend(itemElement);
+        // listElement.prepend(itemElement);
         
-    
-        // inputElement.removeAttribute("disabled");
-        // inputElement.focus();
-        console.log(inputElement)
+        // console.log(inputElement)
+
+        // if the items has any items that is complete. the item will move to the top of list, otherwise any uncomplete new item will be add from buttom of list  
+        if (itemElement.classList.contains('complete')) {
+            listElement.prepend(itemElement); // Move completed items to the top
+        } else {
+            listElement.appendChild(itemElement);
+        }
     
         save();
 
@@ -46,13 +70,18 @@
         const itemElement = document.createElement("div");
         itemElement.classList.add("item");
 
-        const checkbox = document.createElement("input");
-        checkbox.type = "checkbox";
-        checkbox.checked = item.complete;
+        // const checkbox = document.createElement("input");
+        // checkbox.type = "checkbox";
+        // checkbox.checked = item.complete;
 
-        if (item.complete) {
-            itemElement.classList.add("complete");
-        }
+        // if (item.complete) {
+        //     itemElement.classList.add("complete");
+        // }
+
+        const circle = document.createElement("div");
+        circle.classList.add("circle");
+
+        
 
         const inputElement = document.createElement("input");
         inputElement.type = "text";
@@ -66,31 +95,47 @@
 
         const removeButtonElement = document.createElement("button");
         removeButtonElement.classList.add("materialIcons","removeButton");
-        removeButtonElement.innerText = "removeCircle";
+        removeButtonElement.innerText = "remove";
 
        
         actionElement.append(removeButtonElement);
 
-        itemElement.append(checkbox);
+        // itemElement.append(checkbox);
+        itemElement.append(circle);
         itemElement.append(inputElement);
         itemElement.append(actionElement);
 
+        if (item.complete) {
+            itemElement.classList.add("complete");
+        }
+
+        
         // Event
 
-        checkbox.addEventListener("change", () => {
-            item.complete = checkbox.checked;
-            if (item.complete) {
-                itemElement.classList.add("complete");
-            }
-            else {
-                itemElement.classList.remove("complete");
-            }
-            save();
-        })
+        // checkbox.addEventListener("change", () => {
+        //     item.complete = checkbox.checked;
+        //     if (item.complete) {
+        //         itemElement.classList.add("complete");
+        //     }
+        //     else {
+        //         itemElement.classList.remove("complete");
+        //     }
+        //     save();
+        // })
+
+
         // test and commnet out after below 3 lines
         inputElement.addEventListener("input", () => {
             item.text = inputElement.value;
         })
+
+        circle.addEventListener("click", () => {
+            item.complete = !item.complete; // Toggle the complete property
+            updateItemAppearance(item, itemElement, circle); // Update the item's appearance
+            save();
+        });
+
+
 
         inputElement.addEventListener("blur", () => {
             inputElement.setAttribute("readonly","");
@@ -117,7 +162,29 @@
             }
         });
 
-       
+       function updateItemAppearance(item, itemElement, circle) {
+    if (item.complete) {
+        itemElement.classList.add("complete");
+        circle.classList.add("complete-circle")
+
+    } else {
+        itemElement.classList.remove("complete");
+        circle.classList.remove("complete-circle")
+    }
+
+
+    if (defaultHideCompleted) {
+        // If it's set to hide completed by default, hide the item. and then put the complete item to top of list 
+        updateListOrder(); 
+        itemElement.classList.add('hidden');
+        
+    }else {
+        // if it's not hidden, then just simple put the completed items to the top of the list 
+        updateListOrder(); 
+    }
+}
+
+
 
         removeButtonElement.addEventListener("click", () => {
             toDo = toDo.filter(t => t.id != item.id);
@@ -127,25 +194,25 @@
             save();
         })
 
-        return { itemElement,inputElement,removeButtonElement };
-
+        // if complete is in show status, and list has any items that is complete. the item will be hidden
+        if (defaultHideCompleted && itemElement.classList.contains('complete')) {
+            listItem.classList.add('hidden');
+        }
 
 
        
+
+
+        
+
+        return { itemElement,inputElement,removeButtonElement };
+
+
+        
+       
     }
 
-    // function createNewToDo (toDoValue) {
-    //     const item = {
-    //         id : new Date().getTime(),
-    //         text : "",
-    //         complete : false
-    //     }
-
-    //     toDo.unshift(item);
-
-    //     const { itemElement, inputElement } = addToDoItem(item);
-
-    // }
+  
 
     function displayToDos() {
         load();
@@ -174,6 +241,24 @@
         }
     }
 
+
+     // after toggle complete,  sort the list item 
+     function updateListOrder(){
+        const completedItems = document.querySelectorAll('.complete');
+        completedItems.forEach(item => listElement.prepend(item))
+    }
+function hideCompleted(){
+        defaultHideCompleted = true; // Set the flag to hide completed items
+        const completedItems = document.querySelectorAll('.complete');
+        completedItems.forEach(item => item.classList.add('hidden'));
+}
+
+function showCompleted(){
+        defaultHideCompleted = false; // Set the flag to show completed items
+        const completedItems = document.querySelectorAll('.complete');
+        completedItems.forEach(item => item.classList.remove('hidden'));
+        updateListOrder(); // Move completed items to the top
+}
 
 
 
