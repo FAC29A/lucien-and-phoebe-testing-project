35253126f6da5442a711e548f23b26117bc2704f
PAZHames <phoebehames292@gmail.com>
2023-11-08 12:44:53 +0000

    created test for CreateNewToDo function
    test1.js:
    - created test for CreateNewToDo function
    script.js:
    - changed createBtnElement to todoInput field to better reflect usage

--- javascript/test1.js
+++ javascript/test1.js
@@ -1,6 +1,38 @@
 
-// Lucien trial to use TDD method to build function 
+test("CreateTodoElement Converts Text Property to Input Element Value", ()=>{
+const item = {
+    id: 123,
+    text: "To Do Test",
+    complete: false
+}
 
-test("completed items to push to the top of list ", ()=>{
-const result = completedToTop()})
+const expected = item.text;
+const { inputElement } = CreateTodoElement(item);
+const resultText = inputElement.value;
+
+equal(expected,resultText,"CreateTodoElement Converts Text Property to Input Element Value");
+
+})
+
+test("CreateNewTodo should add a new to-do item when Enter key is pressed and input is not empty", ()=> {
+    // command to press enter key
+    // expected result
+    // actual result
+    // equal function
+
+    const mockEvent = {
+        key: "Enter"
+    }
+
+    todoInputField.value = "New To Do Item";
+
+    const initialToDoLength = toDo.length;
+
+    CreateNewTodo(mockEvent);
+
+    equal(toDo.length, initialToDoLength +1, "a new item is added to the toDo list");
+    equal(todoInputField.value, '', "the input field is cleared after adding a new item");
+
+})
+
 
